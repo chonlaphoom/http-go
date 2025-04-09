b0d9e591a4049efa8e58289fd8f02fdce730f692
chonlaphoom <chonlaphoomth@gmail.com>
2025-04-09 23:26:22 +0700

    listen on port 8080

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	// some code
+	fmt.Println("starting server...")
+
+	serverMux := http.NewServeMux()
+	server := &http.Server{
+		Addr: ":8080",
+	}
+
+	err := http.ListenAndServe(server.Addr, serverMux)
+	if err != nil {
+		fmt.Println("Error starting server", err)
+		return
+	} else {
+		fmt.Println("Server started successfully")
+		fmt.Println("Listening on port 8080")
+	}
 }
