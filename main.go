04d7f2ae06a6e5c245494de2df62e2e88815ef7e
chonlaphoom <chonlaphoomth@gmail.com>
2025-04-09 23:34:01 +0700

    refacgtor

--- main.go
+++ main.go
@@ -2,23 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("starting server...")
 
-	serverMux := http.NewServeMux()
+	port := "8080"
+	mux := http.NewServeMux()
 	server := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
+		Handler: mux,
 	}
 
-	err := http.ListenAndServe(server.Addr, serverMux)
-	if err != nil {
-		fmt.Println("Error starting server", err)
-		return
-	} else {
-		fmt.Println("Server started successfully")
-		fmt.Println("Listening on port 8080")
-	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
