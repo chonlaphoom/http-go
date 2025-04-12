4c7bbc37135ee7e074054bab1e833355db78795f
chonlaphoom <chonlaphoomth@gmail.com>
2025-04-12 16:22:47 +0700

    feat: add app hits

--- main.go
+++ main.go
@@ -4,17 +4,75 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+type HitCounter interface {
+	middlewareMetricInt(next http.Handler) http.Handler
+	getHits() int32
+	resetHits()
+}
+
+func (cfg *apiConfig) middlewareMetricInt(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getHits() int32 {
+	return cfg.fileserverHits.Load()
+}
+
+func (cfg *apiConfig) resetHits() {
+	cfg.fileserverHits.Store(0)
+}
+
 func main() {
+	port := "8080"
+	address := ":" + port
+
 	fmt.Println("starting server...")
 
-	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	apiConfig := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	fileServerHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+
+	// handlers
+	mux.Handle("/app/", apiConfig.middlewareMetricInt(fileServerHandler))
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		numberOfHits := apiConfig.getHits()
+
+		_, err := w.Write(fmt.Appendf(nil, "Hits: %d", numberOfHits))
+		if err != nil {
+			log.Fatal("error writing response body")
+		}
+	})
+	mux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+		apiConfig.resetHits()
+		w.WriteHeader(http.StatusOK)
+	})
+
+	// health check
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Fatal("error writing response body")
+		}
+	})
 
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    address,
 		Handler: mux,
 	}
 
